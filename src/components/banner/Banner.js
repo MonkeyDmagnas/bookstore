564c2d31f19aa3db50642e9ebe4cd5ed76b1fa23
Rabbit Studio <wrathlace@gmail.com>
2022-03-04 10:33:43 +0545

    created header

---
+++ src/components/banner/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './banner.css';
+
+export default function Banner() {
+  return (
+    <div className='bg'>
+        <div className='container'>
+            <h1 class="index">Welcome to IT Bookstore</h1>
+            <h2 class="index">IT, Programming and Computer Science Books</h2>
+        </div>
+    </div>
+  )
+}
